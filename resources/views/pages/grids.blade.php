f41203619cdfcc9d1680944d5b6c04f81cd51ba2
Daniel Naranjo <dnaranjo@gmail.com>
2017-12-19 11:12:03 -0300

    UI

--- resources/views/pages/grids.blade.php
+++ resources/views/pages/grids.blade.php
@@ -9,63 +9,81 @@
 			<div class="m-content">
 				<!--Begin::Main Portlet-->
 				<div class="row">
-					<div class="col-xl-12">
-						<!--begin:: Widgets/Application Sales-->
-						<div class="m-portlet m-portlet--full-height ">
-							<div class="m-portlet__head">
+                    @forelse ($results as $result)
+                    <div class="col-xl-3">
+                        <div class="m-portlet m-portlet--bordered-semi m-portlet--full-height ">
+							<div class="m-portlet__head m-portlet__head--fit">
 								<div class="m-portlet__head-caption">
-									<div class="m-portlet__head-title">
-										<h3 class="m-portlet__head-text">
-											{{$titulo}}
-										</h3>
+									<div class="m-portlet__head-action">
+										<button type="button" class="btn btn-sm m-btn--pill">
+											Categoria
+										</button>
 									</div>
 								</div>
 							</div>
 							<div class="m-portlet__body">
-                                <div class="row">
-                                    @forelse ($results as $result)
-                					<div class="col-xl-4">
-                                        <div class="m-portlet m-portlet--mobile">
-        									<div class="m-portlet__head">
-        										<div class="m-portlet__head-caption">
-        											<div class="m-portlet__head-title">
-        												<h3 class="m-portlet__head-text">
-        													{{$result->doc_nombre}}
-        												</h3>
-        											</div>
-        										</div>
-        									</div>
-        									<div class="m-portlet__body">
-                                                @if($result->doc_arch1)
-                                                    <i class="fa fa-file-o"></i>
-                                                    <a href="/documentos/{{$result->doc_arch1}}" target="_blank"> {{$result->doc_arch1}}</a><br>
-                                                @endif
-                                                @if($result->doc_arch2)
-                                                    <i class="fa fa-file-o"></i>
-                                                    <a href="/documentos/{{$result->doc_arch2}}" target="_blank"> {{$result->doc_arch2}}</a><br>
-                                                @endif
-                                                @if($result->doc_arch3)
-                                                     <i class="fa fa-file-o"></i>
-                                                    <a href="/documentos/{{$result->doc_arch3}}" target="_blank"> {{$result->doc_arch3}}</a><br>
-                                                @endif
-                                                @if($result->doc_arch4)
-                                                    <i class="fa fa-file-o"></i>
-                                                    <a href="/documentos/{{$result->doc_arch4}}" target="_blank"> {{$result->doc_arch4}}</a><br>
-                                                @endif
-
-        									</div>
-        								</div>
-                                    </div>
-                                    @empty
-                                        <h5>
-                                            No hay documentos disponibles
-                                        </h5>
-                                    @endforelse
-    							</div>
+								<div class="m-widget19">
+									<div class="m-widget19__pic m-portlet-fit--top m-portlet-fit--sides" style="min-height-: 286px">
+										<img src="assets/app/media/img//blog/blog1.jpg" alt="">
+										<h3 class="m-widget19__title m--font-light">
+											{{$result->title}}
+										</h3>
+										<div class="m-widget19__shadow"></div>
+									</div>
+									<div class="m-widget19__content">
+										<div class="m-widget19__header">
+											<div class="m-widget19__user-img">
+												<img class="m-widget19__img" src="assets/app/media/img//users/user1.jpg" alt="">
+											</div>
+											<div class="m-widget19__info">
+												<span class="m-widget19__username">
+													Carol Carvajal
+												</span>
+												<br>
+												<span class="m-widget19__time">
+													Buenos Aires, Argentina
+												</span>
+											</div>
+											<div class="m-widget19__stats">
+												<span class="m-widget19__number m--font-brand">
+													18
+												</span>
+												<span class="m-widget19__comment">
+													Comentarios
+												</span>
+											</div>
+										</div>
+										<div class="m-widget19__body">
+											{{$result->description}}
+										</div>
+									</div>
+									<div class="m-widget19__action">
+										<button type="button" class="btn m-btn--pill btn-secondary m-btn m-btn--hover-brand m-btn--custom">
+											Contactar
+										</button>
+									</div>
+								</div>
 							</div>
 						</div>
-						<!--end:: Widgets/Application Sales-->
-					</div>
+                    </div>
+                    @empty
+                    <div class="col-xl-4">
+                        <div class="m-portlet m-portlet--mobile">
+                            <div class="m-portlet__head">
+                                <div class="m-portlet__head-caption">
+                                    <div class="m-portlet__head-title">
+                                        <h3 class="m-portlet__head-text">
+                                            Lo sentimos
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="m-portlet__body">
+                                No hay paquetes disponibles
+                            </div>
+                        </div>
+                    </div>
+                    @endforelse
 
                     <div class="col-xl-12">
                         <p>
--- resources/views/pages/listado.blade.php
+++ resources/views/pages/listado.blade.php
@@ -4,187 +4,56 @@
 
 @section('content')
 
-@php
-    $rutaactual = explode('.',Route::currentRouteName())[0];
-    if(preg_match("/consorcios.index/i", Route::currentRouteName() ))
-        {$rutanueva = substr ( url()->current(), -1 );}
-    else
-        {$rutanueva = $_id;}
-
-@endphp
 <div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor-desktop m-grid--desktop m-body">
 	<div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver m-container m-container--responsive m-container--xxl m-page__container">
 		<div class="m-grid__item m-grid__item--fluid m-wrapper">
 			<div class="m-content">
 				<!--Begin::Main Portlet-->
 				<div class="row">
-                    @if(Session::has('consorcio') && preg_match( "/expensas/i", url()->current()) )
-                    <!-- consorcio -->
-                    <div class="col-lg-3">
-                        <!--begin::Portlet-->
+                    @forelse ($results as $result)
+                    <div class="col-xl-3">
+                        <div class="m-widget4">
+                            <!--begin::Widget 14 Item-->
+                            <div class="m-widget4__item">
+                                <div class="m-widget4__img m-widget4__img--pic">
+                                    <img src="assets/app/media/img/users/100_4.jpg" alt="">
+                                </div>
+                                <div class="m-widget4__info">
+                                    <span class="m-widget4__title">
+                                        {{$result->name}}
+                                    </span>
+                                    <br>
+                                    <span class="m-widget4__sub">
+                                        Capital Federal
+                                    </span>
+                                </div>
+                                <div class="m-widget4__ext">
+                                    <a href="/user/{{$result->id}}" class="m-btn m-btn--pill m-btn--hover-brand btn btn-sm btn-secondary">
+                                        Seguir
+                                    </a>
+                                </div>
+                            </div>
+                            <!--end::Widget 14 Item-->
+                        </div>
+                    </div>
+                    @empty
+                    <div class="col-xl-3">
                         <div class="m-portlet m-portlet--mobile">
-                            <!-- <div class="m-portlet__head">
+                            <div class="m-portlet__head">
                                 <div class="m-portlet__head-caption">
                                     <div class="m-portlet__head-title">
                                         <h3 class="m-portlet__head-text">
-                                            <img alt="logo" src="/img/building33.png" class="img-responsive " style="height:80px; padding-top:0;"  />
+                                            Lo sentimos
                                         </h3>
                                     </div>
                                 </div>
-                            </div> -->
+                            </div>
                             <div class="m-portlet__body">
-                                <h5><strong>{{ Session::get('consorcio')[0]->inm_nombre }}</strong></h5>
-                                {{ Session::get('consorcio')[0]->inm_direccion }}<br>
-                                E-mail: <a href="mailto:{{ Session::get('consorcio')[0]->inm_email }}">{{ Session::get('consorcio')[0]->inm_email }}</a><br>
-                                Tel. {{ Session::get('consorcio')[0]->inm_telefono }}<br>
-                                Web: <a href="{{ Session::get('consorcio')[0]->inm_web }}" target="_blank">
-                                    {{ Session::get('consorcio')[0]->inm_web }}
-                                </a>
+                                No hay viajeros disponibles
                             </div>
                         </div>
-                        <!--end::Portlet-->
                     </div>
-                    @foreach($payments as $payment)
-                    <div class="col-lg-3">
-                        <!--begin::Portlet-->
-						<div class="m-portlet m-portlet--mobile">
-							<div class="m-portlet__body">
-                                <h5><strong>Formas de pago</strong></h5>
-                                {{ $payment->for_tipo }}<br>
-                                {{ $payment->for_informacion }}
-                                </a>
-							</div>
-						</div>
-						<!--end::Portlet-->
-                    </div>
-                    @endforeach
-                    <!-- consorcio -->
-                    @endif
-					<div class="col-xl-12">
-						<!--begin:: Widgets/Application Sales-->
-						<div class="m-portlet m-portlet--full-height ">
-							<div class="m-portlet__head">
-								<div class="m-portlet__head-caption">
-									<div class="m-portlet__head-title">
-										<h3 class="m-portlet__head-text">
-											<strong>{{$titulo}}</strong>
-										</h3>
-									</div>
-								</div>
-								<div class="m-portlet__head-tools">
-                                    <div class="m-widget11__action m--align-right">
-                                        {{-- $rutaactual --}}
-                                        {{-- Route::currentRouteName() --}}
-                                        {{-- url()->current() --}}
-                                    </div>
-								</div>
-							</div>
-							<div class="m-portlet__body">
-								<div class="tab-content">
-									<!--begin::Widget 11-->
-									<div class="m-widget11">
-
-                                        @if (session('status'))
-                                            <div class="alert alert-success alert-dismissible">
-                                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                                {{ session('status') }}
-                                            </div>
-                                        @endif
-
-										<div class="table-responsive">
-											<!--begin::Table-->
-                                            {{-- Session::get('consorcio')[0]->con_id --}}
-                                            @if (count($results)>0)
-                                            <table class="table">
-                                                <!--begin::Thead-->
-                                                <thead>
-                                                    <?php foreach ($results[0] as $key => $value) : ?>
-                                                        <?php $fields[] = $key; ?>
-                                                    <?php endforeach; ?>
-                                                    <tr>
-                                                        @foreach ($fields as $field)
-                                                        @if (preg_match("/_id/i", $field))
-                                                        <!-- <td>
-                                                            #
-                                                        </td> -->
-                                                        @else
-                                                        <td class="mayusculas">
-                                                            {{ substr ( $field, 4, strlen($field) ) }}
-                                                        </td>
-                                                        @endif
-                                                        @endforeach
-
-                                                    </tr>
-                                                </thead>
-                                                <!--end::Thead-->
-                                                <!--begin::Tbody-->
-                                                <tbody>
-                                                    @forelse ($results as $result)
-                                                    <tr>
-                                                        @foreach ($fields as $f)
-                                                            @if (preg_match("/_id/i", $f))
-                                                            @php
-                                                                $ID = $result->$f;
-                                                            @endphp
-                                                            <!-- <td>
-                                                                <label class="m-checkbox m-checkbox--solid m-checkbox--single m-checkbox--brand">
-                                                                    <input type="checkbox" name="item" value="{{ $result->$f }}" onclick="javascript:move('/{{ $rutaactual }}/{{ $ID }}/edit')" >
-                                                                    <span></span>
-                                                                </label>
-                                                            </td> -->
-                                                            @else
-                                                            <td>
-                                                                @if(preg_match("/fecha/i", $f))
-                                                                    {{ Carbon\Carbon::parse($result->$f)->format('d/m/Y') }}
-                                                                @elseif(preg_match("/web/i", $f))
-                                                                    @if($result->$f!='')
-                                                                        <a href="//{{ $result->$f }}/?ref=tuconsorcio.com.ar" target="_blank">Visitar</a>
-                                                                    @endif
-                                                                @elseif(preg_match("/email/i", $f))
-                                                                    <a href="mailto:{{ $result->$f }}?subject=Contacto+via+tuconsorcio.com.ar" target="_blank">E-mail</a>
-                                                                @elseif(preg_match("/enviado/i", $f) || preg_match("/leido/i", $f))
-                                                                    @if($result->$f==1)
-                                                                        Si
-                                                                    @else
-                                                                        No
-                                                                    @endif
-                                                                @else
-                                                                    @if ($result->$f=='')
-                                                                        {{''}}
-                                                                    @else
-                                                                        {!! $result->$f !!}
-                                                                    @endif
-                                                                @endif
-                                                            </td>
-                                                            @endif
-                                                        @endforeach
-                                                    </tr>
-                                                    @empty
-                                        				<p>No hay registros para mostrar</p>
-                                        			@endforelse
-                                                </tbody>
-                                                <!--end::Tbody-->
-                                            </table>
-                                            @else
-                                                <p>No hay registros para mostrar</p>
-                                            @endif
-                                            <p>
-                                                {{ $results->links() }}
-                                                @if( !preg_match( "/index/i", Route::currentRouteName()) && !preg_match( "/expensas/i", url()->current()))
-                                                    <a class="btn m-btn--pill btn-secondary m-btn m-btn--custom" href="javascript:history.back();">
-                                                        Volver
-                                                    </a>
-                                                @endif
-                                            </p>
-											<!--end::Table-->
-										</div>
-									</div>
-									<!--end::Widget 11-->
-								</div>
-							</div>
-						</div>
-						<!--end:: Widgets/Application Sales-->
-					</div>
+                    @endforelse
 				</div>
 				<!--End::Main Portlet-->
 			</div>
